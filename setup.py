bacc9d74e177e68db2fb51b3d072f38a85f1c210
Ross Yeager <ryeager12@gmail.com>
2024-01-27 11:22:51 -0800

    packaging it all up!

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import find_packages, setup
 
+required = []
+with open("requirements.txt", encoding="utf-8") as infile:
+    required = infile.read().splitlines()
+
 setup(
-    name="my_package",
+    name="ryutils",
     version="1.0.0",
-    description="A description of my package",
-    author="Your Name",
-    author_email="your@email.com",
+    description="A collection of utilities for Python",
+    author="Ross Yeager",
+    author_email="ryeager12@email.com",
     packages=find_packages(),
-    install_requires=[
-        # List any dependencies your package requires
-    ],
+    install_requires=required,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
