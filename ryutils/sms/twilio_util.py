63fd1156db0b6740b3304352c98f123e013ab40e
Ross Yeager <ryeager12@gmail.com>
2025-01-18 00:54:22 -0800

    fix deprecated

--- ryutils/sms/twilio_util.py
+++ ryutils/sms/twilio_util.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import typing as T
+from datetime import timezone
 
 import pytz
 from twilio.rest import Client
@@ -63,7 +64,7 @@ class TwilioUtil:
         self,
         to_number: str,
         content: str,
-        now: datetime.datetime = datetime.datetime.utcnow(),
+        now: datetime.datetime = datetime.datetime.now(timezone.utc),
     ) -> None:
         if to_number not in self.message_queue:
             self.message_queue[to_number] = []
@@ -89,7 +90,7 @@ class TwilioUtil:
             log.print_bold(f"Sent SMS: {message.sid} - {content}")
 
     def check_sms_queue(
-        self, to_number: str, now: datetime.datetime = datetime.datetime.utcnow()
+        self, to_number: str, now: datetime.datetime = datetime.datetime.now(timezone.utc)
     ) -> None:
         if to_number in self.window:
             start_time = self.window[to_number].get("start_time", 60 * 60 * 8)
