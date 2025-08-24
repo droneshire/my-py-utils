b2d976b6382858b5db6b1eeb39374510a73da559
Ross Yeager <ryeager12@gmail.com>
2025-08-24 16:15:51 -0700

    updated to remove log paths

--- ryutils/path_util.py
+++ ryutils/path_util.py
@@ -1,40 +1,5 @@
 import os
 import traceback
-from pathlib import Path
-
-LOGS_DIR_NAME = "logs"
-CONFIG_DIR_NAME = "config"
-DATA_DIR_NAME = "data"
-NOTEBOOKS_DIR_NAME = "notebooks"
-
-
-def get_root_dir() -> Path:
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    src_dir = os.path.dirname(this_dir)
-    root_dir = os.path.dirname(src_dir)
-    return Path(root_dir)
-
-
-def get_logging_dir(name: str, create_if_not_exist: bool = True) -> Path:
-    root_dir = get_root_dir()
-    log_dir = os.path.join(root_dir, LOGS_DIR_NAME, name)
-
-    if create_if_not_exist:
-        Path(log_dir).mkdir(parents=True, exist_ok=True)
-    return Path(log_dir)
-
-
-def get_config_dir() -> Path:
-    """Get the config directory."""
-    root_dir = get_root_dir()
-    return Path(os.path.join(root_dir, CONFIG_DIR_NAME))
-
-
-def get_data_dir() -> Path:
-    """Get the data directory."""
-    root_dir = get_root_dir()
-    return Path(os.path.join(root_dir, NOTEBOOKS_DIR_NAME, DATA_DIR_NAME))
-
 
 def get_module_name(file_path: str, layers: int = 1) -> str:
     """
